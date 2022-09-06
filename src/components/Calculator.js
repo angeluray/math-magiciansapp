c100bede7c6c3a5474864272c1d6e5ca97a09319
angeluray <aeucampos@gmail.com>
2022-09-06 19:01:29 -0400

    Add class based component

---
+++ src/components/Calculator.js
@@ -0,0 +1,34 @@
+import React from 'react';
+// eslint-disable-next-line react/prefer-stateless-function
+class MyCalculator extends React.Component {
+  render() {
+    return (
+      <div className="calculatorContainer">
+        <div className="resultScreen">0</div>
+        <div className="calculatorButtons">
+          <button type="button">AC</button>
+          <button type="button">+/-</button>
+          <button type="button">%</button>
+          <button type="button">÷</button>
+          <button type="button">7</button>
+          <button type="button">8</button>
+          <button type="button">9</button>
+          <button type="button">x</button>
+          <button type="button">4</button>
+          <button type="button">5</button>
+          <button type="button">6</button>
+          <button type="button">-</button>
+          <button type="button">1</button>
+          <button type="button">2</button>
+          <button type="button">3</button>
+          <button type="button">+</button>
+          <button type="button">0</button>
+          <button type="button">.</button>
+          <button type="button">=</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MyCalculator;
