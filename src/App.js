52822ea87e75ecf7016c9600311cd256f42f177d
angeluray <aeucampos@gmail.com>
2022-09-06 19:41:13 -0400

    Fix class issues

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import React from 'react';
 import './App.css';
 import MyCalculator from './components/Calculator';
 
-function App() {
-  return (
-    <div className="App">
-      <MyCalculator />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <MyCalculator />
+      </div>
+    );
+  }
 }
 
 export default App;
