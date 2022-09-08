316f721bfd626d837bfa813a3b152d46e81139cd
angeluray <aeucampos@gmail.com>
2022-09-08 16:43:40 -0400

    Add refactoring using hooks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,47 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate'; // eslint-disable-line
 
 // eslint-disable-next-line react/prefer-stateless-function
-class MyCalculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-    this.eventHandlerCalc = this.eventHandlerCalc.bind(this);
-  }
+function MyCalculator() {
+  const [myState, mySetStt] = useState({ total: 0, operation: '', next: '' });
 
-  eventHandlerCalc(e) {
-    this.setState((state) => calculate(state, e.target.textContent));
-  }
+  const { total, operation, next } = myState;
 
-  render() {
-    const { total, operation, next } = this.state;
-    return (
-      <div className="calculatorContainer">
+  const eventHandlerCalc = (e) => {
+    mySetStt((myState) => calculate(myState, e.target.textContent));
+  };
+
+  return (
+    <div className="calculatorContainer">
         <div className="resultScreen">{(total ? total : '') + (operation ? operation : '') + (next ? next : '')}</div> {/* eslint-disable-line */}
-        <div className="calculatorButtons">
-          <button type="button" onClick={this.eventHandlerCalc}>AC</button>
-          <button type="button" onClick={this.eventHandlerCalc}>+/-</button>
-          <button type="button" onClick={this.eventHandlerCalc}>%</button>
-          <button type="button" onClick={this.eventHandlerCalc}>÷</button>
-          <button type="button" onClick={this.eventHandlerCalc}>7</button>
-          <button type="button" onClick={this.eventHandlerCalc}>8</button>
-          <button type="button" onClick={this.eventHandlerCalc}>9</button>
-          <button type="button" onClick={this.eventHandlerCalc}>x</button>
-          <button type="button" onClick={this.eventHandlerCalc}>4</button>
-          <button type="button" onClick={this.eventHandlerCalc}>5</button>
-          <button type="button" onClick={this.eventHandlerCalc}>6</button>
-          <button type="button" onClick={this.eventHandlerCalc}>-</button>
-          <button type="button" onClick={this.eventHandlerCalc}>1</button>
-          <button type="button" onClick={this.eventHandlerCalc}>2</button>
-          <button type="button" onClick={this.eventHandlerCalc}>3</button>
-          <button type="button" onClick={this.eventHandlerCalc}>+</button>
-          <button type="button" onClick={this.eventHandlerCalc}>0</button>
-          <button type="button" onClick={this.eventHandlerCalc}>.</button>
-          <button type="button" onClick={this.eventHandlerCalc}>=</button>
-        </div>
+      <div className="calculatorButtons">
+        <button type="button" onClick={eventHandlerCalc}>AC</button>
+        <button type="button" onClick={eventHandlerCalc}>+/-</button>
+        <button type="button" onClick={eventHandlerCalc}>%</button>
+        <button type="button" onClick={eventHandlerCalc}>÷</button>
+        <button type="button" onClick={eventHandlerCalc}>7</button>
+        <button type="button" onClick={eventHandlerCalc}>8</button>
+        <button type="button" onClick={eventHandlerCalc}>9</button>
+        <button type="button" onClick={eventHandlerCalc}>x</button>
+        <button type="button" onClick={eventHandlerCalc}>4</button>
+        <button type="button" onClick={eventHandlerCalc}>5</button>
+        <button type="button" onClick={eventHandlerCalc}>6</button>
+        <button type="button" onClick={eventHandlerCalc}>-</button>
+        <button type="button" onClick={eventHandlerCalc}>1</button>
+        <button type="button" onClick={eventHandlerCalc}>2</button>
+        <button type="button" onClick={eventHandlerCalc}>3</button>
+        <button type="button" onClick={eventHandlerCalc}>+</button>
+        <button type="button" onClick={eventHandlerCalc}>0</button>
+        <button type="button" onClick={eventHandlerCalc}>.</button>
+        <button type="button" onClick={eventHandlerCalc}>=</button>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default MyCalculator;
